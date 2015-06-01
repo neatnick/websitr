e82586c8018ecfe17cc72dbb814ba10893c8865c
unknown <nbalboni2@gmail.com>
2015-06-01 16:37:50 -0400

    Just some more code formatting

--- site_builder.py
+++ site_builder.py
@@ -120,30 +120,45 @@ STYLES_SASS_TEMPLATE = MyTemplate("""\
 
 
 UPDATE_SASS_TEMPLATE = MyTemplate("""\
-import urllib.request
-import shutil
+from urllib.request import urlopen
+from shutil import copyfileobj
 import os
 
 
 RESOURCES = (
-[ { "name": "_flex-box_mixins.scss",
-    "url": "https://raw.githubusercontent.com/mastastealth/sass-flex-mixin/master/_flexbox.scss" },
+[ 
+    { 
+        "name": "_flex-box_mixins.scss",
+        "url":( "https://raw.githubusercontent.com/"
+                "mastastealth/sass-flex-mixin/master/_flexbox.scss" ) 
+    },
 
-  { "name": "_media-query_mixins.scss",
-    "url": "https://raw.githubusercontent.com/paranoida/sass-mediaqueries/master/_media-queries.scss" },
+    { 
+        "name": "_media-query_mixins.scss",
+        "url":( "https://raw.githubusercontent.com/"
+                "paranoida/sass-mediaqueries/master/_media-queries.scss" )
+    },
 
-  { "name": "_general_mixins.scss",
-    "url": "https://raw.githubusercontent.com/SwankSwashbucklers/some-sassy-mixins/master/mixins.scss" } ]
+    { 
+        "name": "_general_mixins.scss",
+        "url":( "https://raw.githubusercontent.com/"
+                "SwankSwashbucklers/some-sassy-mixins/master/mixins.scss" ) 
+    } 
+]
 )
 
 def populate_resource(resource_name, resource_url):
     try:
-        with urllib.request.urlopen(resource_url) as response, open(resource_name, 'wb') as f:
-            shutil.copyfileobj(response, f)
+        with urlopen(resource_url) as response, \\ 
+                open(resource_name, 'wb') as f:
+            copyfileobj(response, f)
         print("Successfully populated '{}'".format(resource_name))
     except Exception as e:
-        message = "Could not populate resource" if not (os.path.isfile(resource_name)) else "Unable to update resource"
-        print("{}: {}\\n  from url: {}\\nException: {}".format(message, resource_name, resource_url, e))
+        message = "Could not populate resource" \\
+            if not (os.path.isfile(resource_name)) \\
+            else "Unable to update resource"
+        print("{}: {}\\n  from url: {}\\nException: {}".format(
+            message, resource_name, resource_url, e ) )
 
 
 print("Updating external sass resources")
